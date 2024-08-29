1c5d505d2e36897f4f98bf04d28dd255c0386451
Aayush Gupta <guptaaayush432@gmail.com>
2024-08-29 16:25:36 +0530

    added @audit-info

--- src/interfaces/IFlashLoanReceiver.sol
+++ src/interfaces/IFlashLoanReceiver.sol
@@ -1,20 +1,23 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity 0.8.20;
 
-import { IThunderLoan } from "./IThunderLoan.sol";
+// @audit-info: unused import
+// it's bad practiice to edit live code for tests/mock
+// import { IThunderLoan } from "./IThunderLoan.sol";
 
 /**
  * @dev Inspired by Aave:
  * https://github.com/aave/aave-v3-core/blob/master/contracts/flashloan/interfaces/IFlashLoanReceiver.sol
  */
 interface IFlashLoanReceiver {
+    // @audit-info Add the natspec?
+    // q what is the token, the token that's being borrowed ?
+    // q amount is the amount of the tokens?
     function executeOperation(
         address token,
         uint256 amount,
         uint256 fee,
         address initiator,
         bytes calldata params
-    )
-        external
-        returns (bool);
+    ) external returns (bool);
 }
